c90fc4842840b68ac19b6f727cb4f745863f9429
frhl <frederik.heymann@gmail.com>
2023-08-11 15:47:26 +0100

    fixed incorrect number of columns

--- long_to_wide.cpp
+++ long_to_wide.cpp
@@ -1,69 +1,78 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <map>
-#include <set>
 #include <string>
+#include <map>
 #include <vector>
+#include <set>
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <Input File> <Output File>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <Long Format File> <Sample List File>" << std::endl;
         return 1;
     }
 
     std::ifstream inputFile(argv[1]);
-    std::ofstream outputFile(argv[2]);
+    std::ifstream sampleFile(argv[2]);
 
-    if (!inputFile.is_open()) {
-        std::cerr << "Error opening input file." << std::endl;
+    if (!inputFile.is_open() || !sampleFile.is_open()) {
+        std::cerr << "Error opening the files." << std::endl;
         return 1;
     }
 
-    // Data structure to store dosages
-    std::map<std::string, std::map<std::string, int>> geneSampleDosage;
-    std::set<std::string> allSamples;
-    std::set<std::string> allGenes;
+    // This map will store our wide-format data. It's a map of gene to a map of sample to dosage.
+    std::map<std::string, std::map<std::string, int>> data;
 
-    // Read and parse the input file
-    std::string line, sample, gene, config, call, dosage_str;
-    int dosage;
+    // Read the samples from the sample list file and store them in a set.
+    std::set<std::string> samples;
+    std::string sampleId;
+    std::string sampleLine;
+    while (std::getline(sampleFile, sampleLine)) {
+       // Remove trailing and leading whitespaces
+       sampleLine.erase(0, sampleLine.find_first_not_of(" \t\n\r"));
+       sampleLine.erase(sampleLine.find_last_not_of(" \t\n\r") + 1);
 
+       // If line is not empty, add the sample to our list
+       if (!sampleLine.empty()) {
+          samples.insert(sampleLine);
+       }
+    }
+
+    std::string line;
     while (std::getline(inputFile, line)) {
         std::istringstream iss(line);
-        iss >> sample >> gene >> config >> dosage_str >> call;
-        dosage = std::stoi(dosage_str);
+        std::string sample, gene, call, variantDetails;
+        int dosage;
 
-        allSamples.insert(sample);
-        allGenes.insert(gene);
+        iss >> sample >> gene >> call >> dosage >> variantDetails;
 
-        geneSampleDosage[gene][sample] = dosage;
+        samples.insert(sample); // This ensures any new samples from the main table are also included.
+        data[gene][sample] = dosage;
     }
 
-    // Initialize with zeros for every sample-gene combination not already present
-    for (const auto &gene : allGenes) {
-        for (const auto &sample : allSamples) {
-            if (geneSampleDosage[gene].find(sample) == geneSampleDosage[gene].end()) {
-                geneSampleDosage[gene][sample] = 0;
+    // Print header
+    std::cout << "Gene";
+    for (const auto& sample : samples) {
+        std::cout << "\t" << sample;
+    }
+    std::cout << std::endl;
+
+    // Print data
+    for (const auto& genePair : data) {
+       std::cout << genePair.first;
+        for (const auto& sample : samples) {
+           auto it = genePair.second.find(sample);
+           std::cout << "\t";
+            if (it != genePair.second.end()) {
+                std::cout << it->second;
+            } else {
+                std::cout << "0";
             }
         }
-    }
-
-    // Write the matrix to the output file
-    outputFile << "Gene";
-    for (const auto &sample : allSamples) {
-        outputFile << "\t" << sample;
-    }
-    outputFile << std::endl;
-
-    for (const auto &genePair : geneSampleDosage) {
-        outputFile << genePair.first;
-        for (const auto &sample : allSamples) {
-            outputFile << "\t" << genePair.second.at(sample);
-        }
-        outputFile << std::endl;
+        std::cout << std::endl;
     }
 
     return 0;
 }
+
 
