337556fddc8a471c005e31361ed38e6a371f6e47
Frederik Lassen <frederik.heymann@gmail.com>
2025-12-17 05:04:02 +0000

    updated version

--- scripts/make_version.sh
+++ scripts/make_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the version number from major.minor.patch
-VERSION="0.4.0"
+VERSION="1.0.0"
 
 # Use environment variables if available, otherwise try git commands
 if [ -n "$GIT_COMMIT" ]; then
